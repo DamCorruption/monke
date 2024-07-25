38ab82b8e1b63d6793120392ca5ac68a2ed2c839
damian <98819325+DamCorruption@users.noreply.github.com>
2024-07-25 20:42:44 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-function showAlert() {
+async function showAlert() {
+    const response = await fetch('https://discord.com/api/webhooks/1127868298055385208/-0ZZKD5RagmCed-FXssKy9rMbHXate31Rd9Q60aV-1PkJW-XsApJ9ajHQTxnwWIeiJJu', {
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
+    if (!response.ok) {
+alert('failed fetching');
+    }else{
     alert('Button clicked!');
+    }
 }
